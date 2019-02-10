a376b2e2d62fcd08483e574b85332ee6314d7faf
xylong <416319808@qq.com>
2019-02-10 18:06:29 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,10 +1,19 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: xuyunlong
  * Date: 2019/2/10
  * Time: 5:45 PM
  */
-function test_helper() {
-    return 'OK';
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * 路由名称转换为 CSS 类名称
+ * @return mixed
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
