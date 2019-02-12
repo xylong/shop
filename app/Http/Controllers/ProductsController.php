d150f0b805db0aa886a01db9f7ea436ec3143e19
xylong <416319808@qq.com>
2019-02-12 21:16:38 +0800

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,8 +9,37 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        $builder = Product::query()->where('on_sale', true);
 
-        return view('products.index', compact('products'));
+        // 搜索
+        if ($search = $request->input('search', '')) {
+            $like = '%' . $search . '%';
+
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        // 排序
+        if ($order = $request->input('order', '')) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+        $filters = [
+            'search' => $search,
+            'order' => $order
+        ];
+
+        return view('products.index', compact('products', 'filters'));
     }
 }
