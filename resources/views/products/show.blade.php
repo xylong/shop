86e6ac9ff380799e21ff11396c959223a276222e
xylong <416319808@qq.com>
2019-02-23 18:16:04 +0800

    商品加入购物车

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -91,17 +91,17 @@
                 axios.post('{{ route('products.favor', ['product' => $product->id]) }}')
                     .then(function () { // 请求成功会执行这个回调
                         swal('操作成功', '', 'success')
-                        .then(function () {
-                            location.reload();
-                        });
-                    }, function(error) { // 请求失败会执行这个回调
+                            .then(function () {
+                                location.reload();
+                            });
+                    }, function (error) { // 请求失败会执行这个回调
                         // 如果返回码是 401 代表没登录
                         if (error.response && error.response.status === 401) {
                             swal('请先登录', '', 'error');
                         } else if (error.response && error.response.data.msg) {
                             // 其他有 msg 字段的情况，将 msg 提示给用户
                             swal(error.response.data.msg, '', 'error');
-                        }  else {
+                        } else {
                             // 其他情况应该是系统挂了
                             swal('系统错误', '', 'error');
                         }
@@ -116,6 +116,41 @@
                                 location.reload();
                             });
                     });
+            });
+
+            // 加入购物车按钮点击事件
+            $('.btn-add-to-cart').click(function () {
+
+                // 请求加入购物车接口
+                axios.post('{{ route('cart.add') }}', {
+                    sku_id: $('label.active input[name=skus]').val(),
+                    amount: $('.cart_amount input').val(),
+                })
+                    .then(function () { // 请求成功执行此回调
+                        swal('加入购物车成功', '', 'success');
+                    }, function (error) { // 请求失败执行此回调
+                        if (error.response.status === 401) {
+
+                            // http 状态码为 401 代表用户未登陆
+                            swal('请先登录', '', 'error');
+
+                        } else if (error.response.status === 422) {
+
+                            // http 状态码为 422 代表用户输入校验失败
+                            var html = '<div>';
+                            _.each(error.response.data.errors, function (errors) {
+                                _.each(errors, function (error) {
+                                    html += error + '<br>';
+                                })
+                            });
+                            html += '</div>';
+                            swal({content: $(html)[0], icon: 'error'})
+                        } else {
+
+                            // 其他情况应该是系统挂了
+                            swal('系统错误', '', 'error');
+                        }
+                    })
             });
         });
     </script>
--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,6 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    // 购物车
+    Route::post('cart', 'CartController@add')->name('cart.add');
 });
